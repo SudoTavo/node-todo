23c82184f19b2997050315945345d44cf9554ed3
Tavo Loaiza <eol@duke.edu>
2019-01-17 01:01:26 -0500

    Changed database info again

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://admin:password@ec2-18-191-30-162.us-east-2.compute.amazonaws.com:27017/dummyDB',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    remoteUrl : 'mongodb://ec2-18-191-30-162.us-east-2.compute.amazonaws.com:27017/dummyDB',
+    localUrl: 'mongodb://ec2-18-191-30-162.us-east-2.compute.amazonaws.com:27017/dummyDB'
 };
